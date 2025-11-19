bcafd3ca68cba2402dbf37f529cff8edebfcb74b
orrans <orrans@gmail.com>
2025-11-19 18:55:08 +0200

    Add filter by address

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -37,7 +37,7 @@ function query() {
     return storageService.query(DB_KEY).then((locs) => {
         if (gFilterBy.txt) {
             const regex = new RegExp(gFilterBy.txt, 'i')
-            locs = locs.filter((loc) => regex.test(loc.name))
+            locs = locs.filter((loc) => regex.test(loc.name) || regex.test(loc.geo.address))
         }
         if (gFilterBy.minRate) {
             locs = locs.filter((loc) => loc.rate >= gFilterBy.minRate)
